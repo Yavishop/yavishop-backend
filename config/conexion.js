4e0dac33106899f3d6f06b098f15d3ba1412b51b
Wsm-naranjo <67296255+Wsm-naranjo@users.noreply.github.com>
2022-08-03 20:38:30 -0500

    cambio en config

--- config/conexion.js
+++ config/conexion.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'admin',
-    password: '1234',
+    password: '',
     port:'3306',
     database: 'ys_db'
 });
